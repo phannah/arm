518b23762e08b6cdcfe589e6db2b8a5a65fc6f63
phannah <peter.hannah@gmail.com>
2023-09-22 09:53:40 -0300

    Add files via upload

---
+++ application-workloads/active-directory/create-ad-forest-with-subdomain/DSC/ConfigureKACE.ps1
@@ -0,0 +1,61 @@
+﻿configuration ConfigureKACE
+{
+   param
+    (
+        [Parameter(Mandatory)]
+        [String]$AgentAzureDownloadPath
+    )
+
+    Node localhost
+    {
+        Script ExecuteInstallKACEAgentForAzure 
+        {
+          SetScript = 
+          {
+	
+	try
+	{
+              Write-Verbose -Message ("Installing KACE agent")
+	      $toolsFolder = "c:\OdradTools"
+
+		if (!(Test-Path $toolsFolder -PathType Container)) {
+		    New-Item -ItemType Directory -Force -Path $toolsFolder
+		}
+
+              $fileName = $toolsFolder + "\KACEInstall.ps1"
+              $updatedFileName = $toolsFolder + "\UpdatedKACEInstall.ps1"
+   
+              Invoke-WebRequest -Uri "$($AgentAzureDownloadPath)" -OutFile $fileName
+              Get-content $fileName | % {$_ -replace "Clear-Host",""} | Out-File $updatedFileName
+              & $updatedFileName          
+	}
+	catch
+	{
+		throw "error From Script: $AgentAzureDownloadPath" + $_.Exception
+	}
+
+          }
+       
+          TestScript = 
+          {
+              Write-Verbose -Message ("Installing KACE agent - TestScript")           
+              if (Test-Path "C:\Program Files (x86)\Quest\KACE")
+              {
+                  Write-Verbose -Message ("KACE already installed")
+                  return $true
+              }
+              else
+              {
+                  Write-Verbose -Message ("KACE agent to be installed")
+                  return $false
+              }
+          }
+        
+          GetScript = 
+          {
+              Write-Verbose -Message ("Installing KACE agent - GetScript")
+          }
+        }   
+        
+    }
+}
